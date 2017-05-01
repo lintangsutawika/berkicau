9872b9074fb598e994b835e16a460d70074f4674
Lintang Sutawika <lintangsutawika@gmail.com>
2017-05-01 19:28:35 +0700

    minor changes

--- rujukKBBI.py
+++ rujukKBBI.py
@@ -53,7 +53,7 @@ class rujukKBBI(object):
 			return "NOTFOUND"
 
 if __name__ == '__main__':
-	kata = "lampu"
+	kata = "menelpon"
 	rujukan = rujukKBBI()
 	rujukan.login()
 	jenis = rujukan.jenis_kata(kata)
