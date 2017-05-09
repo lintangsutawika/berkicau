b5095bf20a69a9eaba37ac5d9fa6e00f80b75a32
Lintang Sutawika <lintangsutawika@gmail.com>
2017-05-09 21:34:24 +0700

    Training dataset is correct and Bi-LSTM works

--- entityTagger.py
+++ entityTagger.py
@@ -1,3 +1,7 @@
+import glob
+import logging
+import logging.handlers
+
 import re
 import nltk
 import POStagger
@@ -6,11 +10,19 @@ import numpy as np
 import gensim
 import csv 
 from sklearn.model_selection import KFold
+import Bi_LSTM
 from Bi_LSTM import BiLSTM_CRF
 import torch
 import torch.autograd as autograd
 import torch.nn as nn
 import torch.optim as optim
+
+LOG_FILENAME = 'NER_logging.out'
+# Set up a specific logger with our desired output level
+# logging = logging.getLogger('logging')
+logging.basicConfig(filemode = 'a', level=logging.DEBUG, filename=LOG_FILENAME)
+
+use_gpu = torch.cuda.is_available()
 
 #Load Trained Tagger 
 posTagger = POStagger.POStagger()
@@ -20,86 +32,103 @@ text = findEntity()
 tags, data = text.corpus2BIO()
 toFeed = []
 for eachSentence in data:
-	toFeed.append(eachSentence)
+    toFeed.append(eachSentence)
 
 #Gain large corpus
 rawSentence = []
 with open("Training/MoreTweets.tsv", 'rU') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter='\n', quotechar='|')
-	for spam in spamreader:
-		rawSentence.append(spam)
+    spamreader = csv.reader(csvfile, delimiter='\n', quotechar='|')
+    for spam in spamreader:
+        rawSentence.append(spam)
 
 corpusSentence =[]
 for individualSentence in rawSentence:
-	if individualSentence == []:
-		pass
-	else:
-		corpusSentence.append(individualSentence[0])
+    if individualSentence == []:
+        pass
+    else:
+        corpusSentence.append(individualSentence[0])
 
 for sentences in corpusSentence:
-	token = nltk.wordpunct_tokenize(sentences.lower())
-	toFeed.append(token)
+    token = nltk.wordpunct_tokenize(sentences.lower())
+    toFeed.append(token)
 
 corpusTweet = toFeed
-word2vec = gensim.models.Word2Vec(corpusTweet, min_count=1,  size=50)
+word2vec = gensim.models.Word2Vec(corpusTweet, min_count=1,  size=100)
+
 #Stemming
 START_TAG = "<START>"
 STOP_TAG = "<STOP>"
-EMBEDDING_DIM = 50
-HIDDEN_DIM = 40
-
+EMBEDDING_DIM = 100
+HIDDEN_DIM = 50
 tag_to_ix = {"None": 0, "I-LOC": 1, "I-ORG": 2, "I-PER":3, START_TAG: 4, STOP_TAG: 5}
 
 model = BiLSTM_CRF(100, tag_to_ix, EMBEDDING_DIM, HIDDEN_DIM)
-optimizer = optim.SGD(model.parameters(), lr=0.01, weight_decay=1e-4)
+optimizer = optim.SGD(model.parameters(), lr=0.001, weight_decay=1e-4, momentum=0.9, nesterov=True)
 
 kf = KFold(n_splits=5)
-for train_index, test_index in kf.split(data):
-	X_train, X_test = np.asarray(data)[train_index], np.asarray(data)[test_index]
-	y_train, y_test = np.asarray(tags)[train_index], np.asarray(tags)[test_index]
+for nFold, (train_index, test_index) in enumerate(kf.split(data)):
+    X_train, X_test = np.asarray(data)[train_index], np.asarray(data)[test_index]
+    y_train, y_test = np.asarray(tags)[train_index], np.asarray(tags)[test_index]
 
-	# model.wv['computer']
-	# Make sure prepare_sequence from earlier in the LSTM section is loaded
-	for epoch in range(10):  # again, normally you would NOT do 300 epochs, it is toy data
-	    for index,sentenceTrain in enumerate(X_train):
-	        # Step 1. Remember that Pytorch accumulates gradients.
-	        # We need to clear them out before each instance
-	        model.zero_grad()
+    # Make sure prepare_sequence from earlier in the LSTM section is loaded
+    for epoch in range(500):  # again, normally you would NOT do 300 epochs, it is toy data
+        totalLoss = 0
+        #Make in to batch of 10?
+        for index,sentenceTrain in enumerate(X_train):
+            optimizer = Bi_LSTM.exp_lr_scheduler(optimizer, epoch)
+            # Step 1. Remember that Pytorch accumulates gradients.
+            # We need to clear them out before each instance
+            model.zero_grad()
 
-	        # Step 2. Get our inputs ready for the network, that is,
-	        # turn them into Variables of word indices.
-	        embedded_sentence = torch.from_numpy(np.asarray([word2vec.wv[w] for w in sentenceTrain]))
-	        targets = torch.LongTensor([t for t in y_train[index]])
+            # Step 2. Get our inputs ready for the network, that is,
+            # turn them into Variables of word indices.
+            embedded_sentence = torch.from_numpy(np.asarray([word2vec.wv[w] for w in sentenceTrain]))
+            targets = torch.LongTensor([t for t in y_train[index]])
 
-	        # Step 3. Run our forward pass.
-	        neg_log_likelihood = model.neg_log_likelihood(autograd.Variable(embedded_sentence), targets)
-	        print("Epoch: {}, sentence_ind: {}, neg_log: {}".format(epoch, index, neg_log_likelihood))
-	        # Step 4. Compute the loss, gradients, and update the parameters by
-	        # calling optimizer.step()
-	        neg_log_likelihood.backward()
-	        optimizer.step()
+            # if use_gpu:
+            #     inputs, labels = autograd.Variable(embedded_sentence.cuda()), autograd.Variable(targets.cuda())
+            # else:
+            inputs, labels = autograd.Variable(embedded_sentence), autograd.Variable(targets)
+            # Step 3. Run our forward pass.
+            neg_log_likelihood = model.neg_log_likelihood(inputs, targets)
+            totalLoss += neg_log_likelihood
+            
+            Log = "nFold: {}, Epoch: {}, sentence_ind: {}, neg_log: {}\n".format(nFold, epoch, index, neg_log_likelihood[0])
+            print(Log)
+            logging.debug(Log)
+            # Step 4. Compute the loss, gradients, and update the parameters by
+            # calling optimizer.step()
+            neg_log_likelihood.backward()
+            optimizer.step()
 
-	    for index,testTrain in enumerate(X_test):
-			embedded_sentence = torch.from_numpy(np.asarray([word2vec.wv[w] for w in testTrain]))
-			printSentence = ""
-			for w in testTrain:
-				printSentence = printSentence + " " + w
-			targets = torch.LongTensor([t for t in y_test[index]])
-			output = model(autograd.Variable(embedded_sentence))[1]
-			print("Sentence:\n{}\nTag:\n{}\nPredict:\n{}".format(printSentence, targets.numpy(), np.asarray(output)))
 
+        print("Loss for this Epoch: {}".format(totalLoss/np.shape(X_train)[0]))
+        confusionMatrix = np.zeros([4,4])
+        for index,testTrain in enumerate(X_test):
+            embedded_sentence = torch.from_numpy(np.asarray([word2vec.wv[w] for w in testTrain]))
+            printSentence = ""
+            for w in testTrain:
+                printSentence = printSentence + " " + w
+
+            targets = torch.LongTensor([t for t in y_test[index]]).numpy()
+            output = np.asarray(model(autograd.Variable(embedded_sentence))[1])
+            for indices, tag in enumerate(targets):
+                confusionMatrix[tag, output[indices]] += 1 
+            # print("Sentence:\n{}\nTag:\n{}\nPredict:\n{}".format(printSentence, targets.numpy(), np.asarray(output)))
+            print("Sentence:\n{}\nTag:\n{}\nPredict:\n{}".format(printSentence, targets, output))
+        
 # We got it!
 #While there are consecutives NNP, make that inot 1 entity
 #if a[i][1] = NNP
 #while a[i][1] == "NNP":
-#	
+#   
 #Argmax distance in word to vec
 # distance.nlevenshtein(method=1)shortest alignment
 #Use for alay words
 #People
-#	-Public figures
-#	-Names
-#	-Characters
+#   -Public figures
+#   -Names
+#   -Characters
 
 #1 MORE DATA FOR WORD EMBEDDING
 #2 BI-LSTM
@@ -116,22 +145,22 @@ false_positive = 0
 false_negative = 0
 
 for predictions in yPred_Person:
-	if (predictions in yPerson) == True:
-		true_positive += 1
-	else:
-		false_positive +=1
+    if (predictions in yPerson) == True:
+        true_positive += 1
+    else:
+        false_positive +=1
 
 for predictions in yPred_Location:
-	if (predictions in yLocation) == True:
-		true_positive += 1
-	else:
-		false_positive +=1
+    if (predictions in yLocation) == True:
+        true_positive += 1
+    else:
+        false_positive +=1
 
 for predictions in yPred_Organization:
-	if (predictions in yOrganization) == True:
-		true_positive += 1
-	else:
-		false_positive +=1
+    if (predictions in yOrganization) == True:
+        true_positive += 1
+    else:
+        false_positive +=1
 
 false_negative = (len(yPerson) + len(yLocation) + len(yOrganization)) - true_positive
 
--- preprocess.py
+++ preprocess.py
@@ -3,214 +3,238 @@ import string
 import nltk
 
 class findEntity(object):
-	"""docstring for findEntity"""
-	def __init__(self, dir="./Training/",filename="training_data_new.txt"):
-		self.corpus = re.split('\r\n',open(dir+filename, "rb").read())
-		self.corpus.pop(-1)
-		self.Types = ["PERSON","LOCATION","ORGANIZATION"]
-		self.person = []
-		self.location = []
-		self.organization = []
-		self.typeDicts = {
-			"PERSON":self.person,
-			"LOCATION":self.location,
-			"ORGANIZATION":self.organization
-			}	
+    """docstring for findEntity"""
+    def __init__(self, dir="./Training/",filename="training_data_new.txt"):
+        self.corpus = re.split('\r\n',open(dir+filename, "rb").read())
+        self.corpus.pop(-1)
+        self.Types = ["PERSON","LOCATION","ORGANIZATION"]
+        self.person = []
+        self.location = []
+        self.organization = []
+        self.typeDicts = {
+            "PERSON":self.person,
+            "LOCATION":self.location,
+            "ORGANIZATION":self.organization
+            }   
 
-	def get_corpus(self):
-		return self.corpus
+    def get_corpus(self):
+        return self.corpus
 
-	def find_enamex(self, corpus=None):
-		if corpus== None:
-			corpus = self.corpus
+    def find_enamex(self, corpus=None):
+        if corpus== None:
+            corpus = self.corpus
 
-		for type in self.Types:
-			start_string = "<ENAMEX TYPE=\"{}\">".format(type)
-			for _temp in corpus:
-				while True:
-					try:
-						start = _temp.index(start_string) + len(start_string)
-						end = start+_temp[start:].index("</ENAMEX>")
-						self.typeDicts["{}".format(type)].append(_temp[start:end])
-						_temp = _temp[end+len("</ENAMEX>"):]
-					except ValueError:
-						break
-		return [self.person, self.location, self.organization]
+        for type in self.Types:
+            start_string = "<ENAMEX TYPE=\"{}\">".format(type)
+            for _temp in corpus:
+                while True:
+                    try:
+                        start = _temp.index(start_string) + len(start_string)
+                        end = start+_temp[start:].index("</ENAMEX>")
+                        self.typeDicts["{}".format(type)].append(_temp[start:end])
+                        _temp = _temp[end+len("</ENAMEX>"):]
+                    except ValueError:
+                        break
+        return [self.person, self.location, self.organization]
 
-	def renameUser(self,corpus=None):
-		_new = []
-		if corpus == None:
-			corpus = self.corpus
-		
-		for _temp in corpus:
-			_temp =  re.sub( r'(^|[^@\w])@(\w{1,15})\b','',_temp)
-			_new.append(_temp)
+    def renameUser(self,corpus=None):
+        _new = []
+        if corpus == None:
+            corpus = self.corpus
+        
+        for _temp in corpus:
+            _temp =  re.sub( r'(^|[^@\w])@(\w{1,15})\b','',_temp)
+            _new.append(_temp)
 
-		return _new
-	
-	def removeEnamex(self,corpus=None):
-		_new = []
-		if corpus == None:
-			corpus = self.corpus
-			
-		for _temp in corpus:
-			_temp =  re.sub( r'</ENAMEX>',' ',_temp)
-			for type in self.Types:
-				_temp =  re.sub( r'<ENAMEX TYPE="{}">'.format(type),' ',_temp)
-			_new.append(_temp)
+        return _new
+    
+    def removeEnamex(self,corpus=None):
+        _new = []
+        if corpus == None:
+            corpus = self.corpus
+            
+        for _temp in corpus:
+            _temp =  re.sub( r'</ENAMEX>',' ',_temp)
+            for type in self.Types:
+                _temp =  re.sub( r'<ENAMEX TYPE="{}">'.format(type),' ',_temp)
+            _new.append(_temp)
 
-		return _new
+        return _new
 
-	def addSpacingEnamex(self,corpus=None):
-		_new = []
-		if corpus == None:
-			corpus = self.corpus
-			
-		for _temp in corpus:
-			_temp =  re.sub( r'</ENAMEX>','</ENAMEX> ',_temp)
-			# for type in self.Types:
-			# 	_temp =  re.sub( r'<ENAMEX TYPE="{}">'.format(type),'<ENAMEX TYPE="{}"> '.format(type),_temp)
-			_new.append(_temp)
+    def addSpacingEnamex(self,corpus=None):
+        _new = []
+        if corpus == None:
+            corpus = self.corpus
+            
+        for _temp in corpus:
+            _temp =  re.sub( r'</ENAMEX>',' _ENAMEX ',_temp)
+            for type in self.Types:
+                _temp =  re.sub( r'<ENAMEX TYPE=\"{}\">'.format(type),'TYPE_{}_ '.format(type),_temp)
+            _new.append(_temp)
 
-		return _new
-	
-	def removeHashtag(self, corpus=None):
-		_new = []
-		if corpus == None:
-			corpus = self.corpus
+        return _new
+    
+    def removeHashtag(self, corpus=None):
+        _new = []
+        if corpus == None:
+            corpus = self.corpus
 
-		for _temp in corpus:
-			_temp = re.sub(r'#(\w+)', '', _temp)
-			_new.append(_temp)
+        for _temp in corpus:
+            _temp = re.sub(r'#(\w+)', '', _temp)
+            _new.append(_temp)
 
-		return _new
+        return _new
 
-	def removeURL(self, corpus=None):
-		#https://support.twitter.com/articles/78124
-		_new = []
-		if corpus == None:
-			corpus = self.corpus
-	
-		for _temp in corpus:
-			_temp = re.sub(r'http:\S+', '', _temp, flags=re.MULTILINE)
-			_temp = re.sub(r'https:\S+', '', _temp, flags=re.MULTILINE)
-			_new.append(_temp)
+    def removeURL(self, corpus=None):
+        #https://support.twitter.com/articles/78124
+        _new = []
+        if corpus == None:
+            corpus = self.corpus
+    
+        for _temp in corpus:
+            _temp = re.sub(r'http:\S+', '', _temp, flags=re.MULTILINE)
+            _temp = re.sub(r'https:\S+', '', _temp, flags=re.MULTILINE)
+            _new.append(_temp)
 
-		return _new
+        return _new
 
-	def removeEmoticon(self, corpus=None):
-		#https://support.twitter.com/articles/78124
-		_new = []
-		emoticons_str = r"(?:[:=;B][oO\-]?[D\)\]\(\]/\\OpP])"
+    def removeEmoticon(self, corpus=None):
+        #https://support.twitter.com/articles/78124
+        _new = []
+        emoticons_str = r"(?:[:=;B\-][oO\_\-]?[\-D\)\]\(\]/\\OpP3])"
 
-		if corpus == None:
-			corpus = self.corpus
-	
-		for _temp in corpus:
-			_temp = re.sub(emoticons_str, '', _temp)
-			_new.append(_temp)
+        if corpus == None:
+            corpus = self.corpus
+    
+        for _temp in corpus:
+            _temp = re.sub(emoticons_str, '', _temp)
+            _new.append(_temp)
 
-		return _new
+        return _new
 
-	def removeSingleLetter(self, corpus=None):
-		pass
+    def removeSingleLetter(self, corpus=None):
+        pass
 
-	def removeRepeatLetter(self, corpus=None):
-		_new = []
-		if corpus == None:
-			corpus = self.corpus
+    def addSpaceToPunctuation(self, corpus=None):
+        _new = []
+        punctuations = list(string.punctuation)
+        if corpus == None:
+            corpus = self.corpus
 
-		for _temp in corpus:
-			repeats = re.findall(r'((\w)\2{1,})', _temp)
-			for repeatGroup in repeats:
-				_temp = re.sub(repeatGroup[0], repeatGroup[1], _temp)
-			
-			_new.append(_temp)
+        for _temp in corpus:
+            for pun in punctuations:
+                _temp = re.sub(pun, " "+pun+" ", _temp)
+            _new.append(_temp)
 
-		return _new
+        return _new
 
-	def removeAll(self,corpus=None):
-		if corpus == None:
-			corpus = self.corpus
-		
-		_temp = self.removeURL()
-		_temp = self.renameUser(_temp)
-		_temp = self.removeEnamex(_temp)
-		_temp = self.removeHashtag(_temp)
-		_temp = self.removeEmoticon(_temp)
-		return _temp
+    def removeRepeatLetter(self, corpus=None):
+        _new = []
+        if corpus == None:
+            corpus = self.corpus
 
-	def corpus2BIO(self, corpus=None):
-		if corpus== None:
-			corpus = self.corpus
+        for _temp in corpus:
+            repeats = re.findall(r'((\w)\2{1,})', _temp)
+            for repeatGroup in repeats:
+                _temp = re.sub(repeatGroup[0], repeatGroup[1], _temp)
+            
+            _new.append(_temp)
 
-		_temp = self.removeURL()
-		_temp = self.renameUser(_temp)
-		_temp = self.removeHashtag(_temp)
-		_temp = self.removeEmoticon(_temp)
-		_temp = self.addSpacingEnamex(_temp)
+        return _new
 
-		tags = []
-		dataset = []
-		for sentences in _temp:
-			sentences = sentences.lower()
-			tempSentence = re.split(' +', sentences)
+    def removeAll(self,corpus=None):
+        if corpus == None:
+            corpus = self.corpus
+        
+        _temp = self.removeURL()
+        _temp = self.renameUser(_temp)
+        # _temp = self.removeEnamex(_temp)
+        _temp = self.removeHashtag(_temp)
+        _temp = self.removeEmoticon(_temp)
+        return _temp
 
-			while '<enamex' in tempSentence:
-				tempSentence.pop(tempSentence.index('<enamex'))
+    def corpus2BIO(self, corpus=None):
+        if corpus== None:
+            corpus = self.corpus
 
-			while '' in tempSentence:
-				tempSentence.pop(tempSentence.index(''))
+        _temp = self.removeURL()
+        _temp = self.renameUser(_temp)
+        _temp = self.removeHashtag(_temp)
+        _temp = self.removeEmoticon(_temp)
+        _temp = self.addSpacingEnamex(_temp)
 
-			punctuations = list(string.punctuation)
-			tempSentence = [word for word in tempSentence if word not in punctuations]
-			
-			tagSentence = []
-			for index, words in enumerate(tempSentence):
-				if "type=\"person\"" in words:
-					# if "</ENAMEX>" in words:
-					tempSentence[index] =  re.sub( "type=\"person\">",'',words)
-					tagSentence.append(1)
-				elif "type=\"location\"" in words:
-					# if "</ENAMEX>" in words:
-					tempSentence[index] =  re.sub( "type=\"location\">",'',words)
-					tagSentence.append(2)
-				elif "type=\"organization\"" in words:
-					# if "</ENAMEX>" in words:
-					tempSentence[index] =  re.sub( "type=\"organization\">",'',words)
-					tagSentence.append(3)
-				elif "</enamex>" in words:
-					tempSentence[index] =  re.sub( "</enamex>",'',words)
-					if index > 0:
-						tagSentence.append(tagSentence[index-1])
-				else:
-					if index > 0 and tagSentence[index-1] != 0 and "</enamex>" not in tempSentence[index-1]: 
-						tagSentence.append(tagSentence[index-1])
-					else:
-						tempSentence[index-1] = re.sub( "</enamex>",'',tempSentence[index-1])
-						tagSentence.append(0)
+        tags = []
+        dataset = []
+        for sentences in _temp:
+            sentences = sentences.lower()
+            tempSentence = nltk.wordpunct_tokenize(sentences.lower())
+            
+            tagSentence = []
+            for index, words in enumerate(tempSentence):
+                if "type_person_" in words:
+                    tempSentence[index] =  re.sub( "type_person_",'',words)
+                    tagSentence.append(1)
+                elif "type_location_" in words:
+                    tempSentence[index] =  re.sub( "type_location_",'',words)
+                    tagSentence.append(2)
+                elif "type_organization_" in words:
+                    tempSentence[index] =  re.sub( "type_organization_",'',words)
+                    tagSentence.append(3)
+                elif index>0 and tempSentence[index-1]=="_enamex" and ("type_person_" not in words or "type_location_" not in words or "type_organization_" not in words):
+                    tagSentence.append(0)
+                    # tempSentence.pop(tempSentence.index("_enamex"))
+                    # tempSentence[index-1] = re.sub( "</enamex>",'',tempSentence[index-1])
+                elif index>0 and tagSentence[index-1] != 0:# and "</enamex>"!=tempSentence[index+1]: 
+                    tagSentence.append(tagSentence[index-1])
+                elif index==0:
+                    tagSentence.append(0)
+                else:
+                    tagSentence.append(0)
 
-			tags.append(tagSentence)
-			dataset.append(tempSentence)
-		return [tags, dataset]
-					# 	else:
-					# if index == 0:
-					# 	tagSentence.append(0)
-					# elif tagSentence[index-1] != 0 and ("</ENAMEX>" in tagSentence[index-1]):
-					# 	tagSentence.append(tagSentence[index-1])
-					# else:
-					# 	tagSentence.append(0)						
-		#TagSET
-		#None :0
-		#I-LOC:1
-		#I-ORG:2
-		#I-PER:3
+
+                # tempSentence[index-1] = re.sub( "</enamex>",'',tempSentence[index-1])
+                #       tagSentence.append(0)
+                # elif "</enamex>" in words:
+                #   tempSentence[index] =  re.sub( "</enamex>",'',words)
+                    # if index > 0:
+                        # tagSentence.append(tagSentence[index-1])
+            while True:
+                if '' in tempSentence:
+                    ind = tempSentence.index('')
+                    tempSentence.pop(ind)
+                    tagSentence.pop(ind)
+                else:
+                    break
+
+            while True:
+                if '_enamex' in tempSentence:
+                    ind = tempSentence.index('_enamex')
+                    tempSentence.pop(ind)
+                    tagSentence.pop(ind)
+                else:
+                    break
+
+            tags.append(tagSentence)
+            dataset.append(tempSentence)
+        return [tags, dataset]
+                    #   else:
+                    # if index == 0:
+                    #   tagSentence.append(0)
+                    # elif tagSentence[index-1] != 0 and ("</ENAMEX>" in tagSentence[index-1]):
+                    #   tagSentence.append(tagSentence[index-1])
+                    # else:
+                    #   tagSentence.append(0)                       
+        #TagSET
+        #None :0
+        #I-LOC:1
+        #I-ORG:2
+        #I-PER:3
 
 
 if __name__ == '__main__':
-	text = findEntity()
-	filtered = text.removeAll()
-	file = open("output.txt", "w")
-	for sentences in filtered:
-		file.write(sentences) 
-	file.close()
+    text = findEntity()
+    filtered = text.removeAll()
+    file = open("output.txt", "w")
+    for sentences in filtered:
+        file.write(sentences) 
+    file.close()
